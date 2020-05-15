0d09ebf1556fc1051cd4b315bb58f197a4e046c3
Fernedy González Arias <fernedy.arias@gmail.com>
2020-05-15 12:28:32 -0500

    Change general v0.5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -757,7 +757,7 @@
                                                     </button>
                                                     <ul class="dropdown-menu dropdown-primary" role="menu">
                                                         <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
-                                                            Logout
+                                                            Logout <i class="entypo-logout"></i>
                                                         </a>
                                                         <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
                                                             {{ csrf_field() }}
