473fc9bff07df30fee7c5f7efebd526c57cbba2f
Fernedy González Arias <fernedy.arias@gmail.com>
2020-05-16 10:54:05 -0500

    Ultimo merge

--- resources/views/instahunters.blade.php
+++ resources/views/instahunters.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('content')
 
     <!-- Breadcrumb -->
@@ -20,27 +21,17 @@
                                 <strong>InstaHunters</strong>
                         </li>
                         </ol>
-<div style="height: 30px;" class=""></div>
-<div class="container page-body boxed-layout">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card shadow-lg p-3 mb-5 bg-white ">
-                <center>
-                <div class="card-header">
-                    <a href="#">
-                        <img src="{{ asset('Imagenes/logo.png') }}" width="185" alt="" />
-                    </a>
                 </div>
             </div>
 
         </div>
     </section>
-<div class="container fondo_container">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header text-center display-4"><h1> InstaHunters <i class="fas fa-spider"></i>
-                </center>
+                </div>
                 <div class="card-header text-center text-primary display-4 ">
                     <a href="https://universitariadecolombia.edu.co/programas/profesionales/ingenieria-de-sistemas/" class="text-primary"> by Semillero Ing. de Sistemas, Universitaria de Colombia.</a></div>
                 <div class="card-body display-4">
@@ -52,11 +43,20 @@
                                 <option value="hashtag">#Hashtag</option>
                             </select>
                             @error('campoSelect')
+                            <div class="alert alert-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                            @enderror
+                        </div>
                         <div class="form-group row card-header text-center display-4">
                             <input type="text" class="form-control form-control-lg" placeholder="Ingrese la palabra clave (username o hashtag)" name="palabraClave">
                             @error('palabraClave')
-                    </div>
-                </div>
+                            <div class="alert alert-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                            @enderror
+                        </div>
+
                         <div class="form-group">
                             <div class="card-header text-center display-4">
                                 <button href="#" type="submit" class="btn btn-success btn-lg float-right" name="buscar">
@@ -66,7 +66,7 @@
                             <a href="{{URL::to('instahunterview')}}" class="btn btn-info float-right btn-lg">
                                 Vista previa <i class="fas fa-eye"></i>
                             </a>
-                    </div>
+                        </div>
                 </div>
             </div>
             <center>
@@ -77,7 +77,6 @@
             </center>
         </div>
     </div>
-</div>
 </div>
 <footer>
     @if ($data2view!=null)
@@ -90,4 +89,3 @@
     @endif
 </footer>
 @endsection
-
--- storage/framework/views/230e74e8a691826d4e079844a07062395a5c53fc.php
+++ storage/framework/views/230e74e8a691826d4e079844a07062395a5c53fc.php
@@ -1,5 +1,8 @@
 <?php $__env->startSection('content'); ?>
 
+
+  
+
     <!-- Breadcrumb -->
     <section class="breadcrumb">
 
@@ -26,144 +29,148 @@
 
         </div>
     </section>
-    <div class="container">
-    <div class="row">
-            <div class="col-md-5">
-                <h2> REGISTRO DE USUARIOS</h2>
-                <div class="form-row">
-            </div>
-        </div>
-    </div>
+    <div class="container fondo_container">
+      <div class="row">
+              <div class="col-md-5">
+                  <h2 style="color:white" > REGISTRO DE USUARIOS</h2>
+                  <div class="form-row">
+              </div>
+          </div>
+      </div>
     <form method="POST" action="<?php echo e(URL::to('GestUserIntCrear/guardar')); ?>"> <?php echo e(csrf_field()); ?>
 
-  <div class="form-row">
-    <div class="col">
-    <label for="">Tipo de Identificaciòn:</label>
-        <select name="ID_TIPO_INDENTIFICACION" id="ID_TIPO_INDENTIFICACION" class="form-control">
-        <option value="">--Escoja Tipo de Identificacion--</option>
-            <?php $__currentLoopData = $TipoIdents; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $TipoIdent): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                <option value="<?php echo e($TipoIdent->NOMBRE_TIPO_INDENTIFICACION); ?>"><?php echo e($TipoIdent->	NOMBRE_TIPO_INDENTIFICACION); ?></option>
-            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </select>
-        <span class="text-danger"><?php echo e($errors->first("ID_TIPO_INDENTIFICACION")); ?></span>
-    </div>
-    <div class="col">
-    <label>Numero de Documento:</label>
-    <input type="text" name="NUM_DOCUMENTO" class="form-control" placeholder="Numero de Documento">
-    <span class="text-danger"><?php echo e($errors->first("NUM_DOCUMENTO")); ?></span>
-    </div>
-  </div>
+            <div class="form-row">
+              <div class="col-sm-6">
+              <label style="color:white" for="">Tipo de Identificaciòn:</label>
+                  <select name="ID_TIPO_INDENTIFICACION" id="ID_TIPO_INDENTIFICACION" class="form-control">
+                  <option value="">--Escoja Tipo de Identificacion--</option>
+                      <?php $__currentLoopData = $TipoIdents; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $TipoIdent): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                          <option value="<?php echo e($TipoIdent->NOMBRE_TIPO_INDENTIFICACION); ?>"><?php echo e($TipoIdent->	NOMBRE_TIPO_INDENTIFICACION); ?></option>
+                      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                  </select>
+                  <span class="text-danger"><?php echo e($errors->first("ID_TIPO_INDENTIFICACION")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white">Numero de Documento:</label>
+              <input type="text" name="NUM_DOCUMENTO" class="form-control" placeholder="Numero de Documento">
+              <span class="text-danger"><?php echo e($errors->first("NUM_DOCUMENTO")); ?></span>
+              </div>
+            </div>
 
-  <div class="form-row">
-    <div class="col">
-    <label>Nombre Completo:</label>
-    <input type="text" name="NOMBRE" class="form-control" placeholder="Nombre Completo">
-    <span class="text-danger"><?php echo e($errors->first("NOMBRE")); ?></span>
-    </div>
-    <div class="col">
-    <label>Direcciòn de Residencia:</label>
-    <input type="text" name="DIRECCION" class="form-control" placeholder="Direcciòn">
-    <span class="text-danger"><?php echo e($errors->first("DIRECCION")); ?></span>
-    </div>
-  </div>
-  <div class="form-row">
-    <div class="col">
-    <label>Telefono:</label>
-    <input type="phone" name="TELEFONO" class="form-control" placeholder="Telefono">
-    <span class="text-danger"><?php echo e($errors->first("TELEFONO")); ?></span>
-    </div>
-    <div class="col">
-    <label>Pais:</label>
-    <input value="COLOMBIA" type="text" name="PAIS" class="form-control" placeholder="Pais">
-    <span class="text-danger"><?php echo e($errors->first("PAIS")); ?></span>
-    </div>
-  </div>
+            <div class="form-row">
+              <div class="col-sm-6">
+              <label style="color:white">Nombre Completo:</label>
+              <input type="text" name="NOMBRE" class="form-control" placeholder="Nombre Completo">
+              <span class="text-danger"><?php echo e($errors->first("NOMBRE")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white">Direcciòn de Residencia:</label>
+              <input type="text" name="DIRECCION" class="form-control" placeholder="Direcciòn">
+              <span class="text-danger"><?php echo e($errors->first("DIRECCION")); ?></span>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="col-sm-6">
+              <label style="color:white">Telefono:</label>
+              <input type="phone" name="TELEFONO" class="form-control" placeholder="Telefono">
+              <span class="text-danger"><?php echo e($errors->first("TELEFONO")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white">Pais:</label>
+              <input value="COLOMBIA" type="text" name="PAIS" class="form-control" placeholder="Pais">
+              <span class="text-danger"><?php echo e($errors->first("PAIS")); ?></span>
+              </div>
+            </div>
 
-  <div class="form-row">
-    <div class="col">
-    <label>Ciudad:</label>
-    <input type="text" name="CIUDAD" class="form-control" placeholder="Ciudad">
-    <span class="text-danger"><?php echo e($errors->first("CIUDAD")); ?></span>
-    </div>
-    <div class="col">
-    <label>Correo Electronico:</label>
-    <input type="email" name="CORREO" class="form-control" placeholder="Correo Electronico">
-    <span class="text-danger"><?php echo e($errors->first("CORREO")); ?></span>
-    </div>
-  </div>
+            <div class="form-row">
+              <div class="col-sm-6">
+              <label style="color:white">Ciudad:</label>
+              <input type="text" name="CIUDAD" class="form-control" placeholder="Ciudad">
+              <span class="text-danger"><?php echo e($errors->first("CIUDAD")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white">Correo Electronico:</label>
+              <input type="email" name="CORREO" class="form-control" placeholder="Correo Electronico">
+              <span class="text-danger"><?php echo e($errors->first("CORREO")); ?></span>
+              </div>
+            </div>
 
-  <div class="form-row">
-    <div class="col">
-    <label>Telefono Oficina:</label>
-    <input type="phone" name="TELEFONO_OFICINA" class="form-control" placeholder="Telefono de Oficina">
-    <span class="text-danger"><?php echo e($errors->first("TELEFONO_OFICINA")); ?></span>
-    </div>
-    <div class="col">
-    <label>Extenciòn:</label>
-    <input type="phone" name="EXTENSION" class="form-control" placeholder="Extensiòn">
-    <span class="text-danger"><?php echo e($errors->first("EXTENSION")); ?></span>
-    </div>
-  </div>
+            <div class="form-row">
+              <div class="col-sm-6">
+              <label style="color:white">Telefono Oficina:</label>
+              <input type="phone" name="TELEFONO_OFICINA" class="form-control" placeholder="Telefono de Oficina">
+              <span class="text-danger"><?php echo e($errors->first("TELEFONO_OFICINA")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white">Extenciòn:</label>
+              <input type="phone" name="EXTENSION" class="form-control" placeholder="Extensiòn">
+              <span class="text-danger"><?php echo e($errors->first("EXTENSION")); ?></span>
+              </div>
+            </div>
 
-  <div class="form-row">
-    <div class="col">
-    <label>Usuario de Registro:</label>
-    <input type="email" name="USUARIO" class="form-control" placeholder="Usuario de registro Correo">
-    <span class="text-danger"><?php echo e($errors->first("USUARIO")); ?></span>
-    </div>
-    <div class="col">
-    <label for="inputEmail4">Password:</label>
-    <input type="password" name="CONTRASENA" class="form-control" placeholder="Escribir Password">
-    <span class="text-danger"><?php echo e($errors->first("CONTRASENA")); ?></span>
-    </div>
-  </div>
+            <div class="form-row">
+              <div class="col-sm-6">
+              <label style="color:white">Usuario de Registro:</label>
+              <input type="email" name="USUARIO" class="form-control" placeholder="Usuario de registro Correo">
+              <span class="text-danger"><?php echo e($errors->first("USUARIO")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white" for="inputEmail4">Password:</label>
+              <input type="password" name="CONTRASENA" class="form-control" placeholder="Escribir Password">
+              <span class="text-danger"><?php echo e($errors->first("CONTRASENA")); ?></span>
+              </div>
+            </div>
 
-  <div class="form-row">
-    <div class="col">
-    <label for="">Cargo del Funcionario:</label>
-        <select name="ID_CARGO" id="ID_CARGO" class="form-control">
-        <option value="">--Escoja Tipo de Cargo--</option>
-            <?php $__currentLoopData = $cargos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $cargo): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                <option value="<?php echo e($cargo->NOMBRE_CARGO); ?>"><?php echo e($cargo->NOMBRE_CARGO); ?></option>
-            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </select>
-        <span class="text-danger"><?php echo e($errors->first("ID_CARGO")); ?></span>
-    </div>
-    <div class="col">
-    <label for="">Rol del Funcionario:</label>
-        <select name="ID_ROL" id="ID_ROL" class="form-control">
-        <option value="">--Escoja Tipo de Rol--</option>
-            <?php $__currentLoopData = $roles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rol): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                <option value="<?php echo e($rol->id); ?>"><?php echo e($rol->name); ?></option>
-            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </select>
-        <span class="text-danger"><?php echo e($errors->first("ID_ROL")); ?></span>
-    </div>
-    <div class="col">
-    <label>Estado:</label>
-    <select name="ESTADO"class="form-control">
-    <option value="">--Escoja Estado--</option>
-            <option value="ACTIVO">ACTIVO</option>
-            <option value="DESACTIVADO">DESACTIVADO</option>
-    </select>
-    <span class="text-danger"><?php echo e($errors->first("ESTADO")); ?></span>
-  </div>
-  </div>
+            <div class="form-row">
+              <div class="col-sm-6">
+              <label style="color:white" for="">Cargo del Funcionario:</label>
+                  <select name="ID_CARGO" id="ID_CARGO" class="form-control">
+                  <option value="">--Escoja Tipo de Cargo--</option>
+                      <?php $__currentLoopData = $cargos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $cargo): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                          <option value="<?php echo e($cargo->NOMBRE_CARGO); ?>"><?php echo e($cargo->NOMBRE_CARGO); ?></option>
+                      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                  </select>
+                  <span class="text-danger"><?php echo e($errors->first("ID_CARGO")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white" for="">Rol del Funcionario:</label>
+                  <select name="ID_ROL" id="ID_ROL" class="form-control">
+                  <option value="">--Escoja Tipo de Rol--</option>
+                      <?php $__currentLoopData = $roles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rol): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                          <option value="<?php echo e($rol->id); ?>"><?php echo e($rol->name); ?></option>
+                      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                  </select>
+                  <span class="text-danger"><?php echo e($errors->first("ID_ROL")); ?></span>
+              </div>
+              <div class="col-sm-6">
+              <label style="color:white">Estado:</label>
+              <select name="ESTADO"class="form-control">
+              <option value="">--Escoja Estado--</option>
+                      <option value="ACTIVO">ACTIVO</option>
+                      <option value="DESACTIVADO">DESACTIVADO</option>
+              </select>
+              <span class="text-danger"><?php echo e($errors->first("ESTADO")); ?></span>
+            </div>
+            </div>
 
-  <div class="form-group">
-    <label>Observaciones:</label>
-    <textarea name="OBSERVACION" class="form-control" placeholder="Escribe tu Observación"></textarea>
-    <span class="text-danger"><?php echo e($errors->first("OBSERVACION")); ?></span>
-  </div>
-  <div class="form-row">
-                    <div class="form-group">
-                        <input type="submit" value="Registrar" class="btn btn-primary">
-                        <a class="btn btn-secondary" href="<?php echo e(URL::to('UsuariosInterno')); ?>">Regresar</a>
-                    </div>
+            <div class="form-group col-sm-6">
+              <label style="color:white">Observaciones:</label>
+              <textarea name="OBSERVACION" class="form-control" placeholder="Escribe tu Observación"></textarea>
+              <span class="text-danger"><?php echo e($errors->first("OBSERVACION")); ?></span>
 
-</form>
-</body>
-</html>
+              
+            </div>
+            <div class="form-row">
+              <div class="form-group">
+                  <input type="submit" value="Registrar" class="btn btn-primary">
+                  <a class="btn btn-secondary" href="<?php echo e(URL::to('UsuariosInterno')); ?>">Regresar</a>
+              </div>
+            </div>
+
+          </form>
+</div>
+
+
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\AppTransaccional-v4.2rc\resources\views/GestUserIntCrear.blade.php ENDPATH**/ ?>
