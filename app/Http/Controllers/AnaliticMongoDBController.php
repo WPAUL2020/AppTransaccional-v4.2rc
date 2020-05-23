119049ed93bc80c7f061af1d4597ac52bfc7a7ed
Fernedy González Arias <fernedy.arias@gmail.com>
2020-05-23 15:00:58 -0500

    Scrap by id user Success!!!

--- app/Http/Controllers/AnaliticMongoDBController.php
+++ app/Http/Controllers/AnaliticMongoDBController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\scrapedUserCollectionMongoDB as scrapedUser;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
 use App\dataCollectionMongoDB as dataCollection;
+use App\scrapedUserCollectionMongoDB as scrapedUser;
 
 class AnaliticMongoDBController extends Controller
 {
+    protected $date;
+
+    public function __construct() {
+        $this->client = new Client([
+            'base_uri' => 'www.instagram.com/'
+        ]);
+        $this->middleware('auth');
+        date_default_timezone_set('America/Bogota');
+        $this->date = date('d/m/Y H:i:s');
+    }
     public function index()
     {
         $data = dataCollection::All();
@@ -20,39 +34,30 @@ class AnaliticMongoDBController extends Controller
 
     private function findByIDUser($id_user)
     {
-        foreach ($id_user as $value) {
-            dd($value->wordSearch);
+        $dataInsert = [
+            'wordSearch' => $id_user[0]['wordSearch'],
+            'consulta_log' => $this->date
+        ];
+
+        $itemCollection = collect($id_user[0]);
+        for ($i=1; $i <= count($itemCollection) ; $i++) {
+            error_reporting(~E_NOTICE);
+            try {
+                $response =  $this->client->request('GET', "p/".$id_user[0][$i]['id_usuario']."/?__a=1");
+                $allData = json_decode($response->getBody()->getContents());
+                array_push($dataInsert, $this->truncateUsername($allData));
+            } catch (ClientException $e) {
+                $e->getResponse()->getStatusCode();
+            }
         }
-
-        $dataInsert = [];
-
-/*         $wordSerach = $id_user['wordSearch'];
-        foreach ($id_user as $id) {
-            error_reporting(~E_WARNING);
-            $id_user= $id['id_usuario'];
-            $res = $this->client->request('POST', 'findByUserID', [
-                'headers' => [
-                'Accept'     => 'application/json',
-                ],
-                'json' => [
-                    'id_usuario' => $id_user,
-                    ]
-                ]
-
-            );
-            $data2view = json_decode($res->getBody()->getContents());
-            array_push($dataInsert, $this->truncateUsername($data2view));
-        }
-
-        array_push($dataInsert, $wordSerach); */
-
+        array_push($dataInsert, $parametros);
         $dataMongoDB = new \App\scrapedUserCollectionMongoDB;
         $dataMongoDB->insert($dataInsert);
     }
 
     private function truncateUsername($data)
     {
-        $routeAtributte = $data->entry_data->PostPage[0]->graphql->shortcode_media->owner;
+        $routeAtributte = $data->graphql->shortcode_media->owner;
         $arrayUsername = [
             'userName' => $routeAtributte->username,
             'fullName' => $routeAtributte->full_name,
