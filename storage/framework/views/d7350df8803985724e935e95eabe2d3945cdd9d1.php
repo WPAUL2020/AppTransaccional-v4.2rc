e2527ab45bb05fef2e2bcf04c22fe966cbc0b0e6
Elce1213 <cpaezlozano@gmail.com>
2020-05-26 15:50:54 -0500

    first commit

--- storage/framework/views/d7350df8803985724e935e95eabe2d3945cdd9d1.php
+++ storage/framework/views/d7350df8803985724e935e95eabe2d3945cdd9d1.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+<?php $__env->startSection('content'); ?>
 
     <!-- Breadcrumb -->
     <section class="breadcrumb">
@@ -15,7 +13,7 @@
 
                     <ol class=" bc-3" >
                             <li>
-                    <a href="{{URL::previous()}}"> <i class="fas fa-angle-left"></i> Regresar</a>
+                    <a href="<?php echo e(URL::previous()); ?>"> <i class="fas fa-angle-left"></i> Regresar</a>
                 </li>
                     <li class="active">
                                 <strong>Gestión Big Data/ Estadistica</strong>
@@ -127,12 +125,12 @@
     </div>
 
 
-@endsection
-@section('Scripts')
-<script src="{{ asset('neonFrontEnd/assets/js/rickshaw/rickshaw.min.js') }}"></script>
-<script src="{{ asset('neonFrontEnd/assets/js/raphael-min.js') }}"></script>
-<script src="{{ asset('neonFrontEnd/assets/js/morris.min.js') }}"></script>
-<script src="{{ asset('neonFrontEnd/assets/js/jquery.peity.min.js') }}"></script>
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('Scripts'); ?>
+<script src="<?php echo e(asset('neonFrontEnd/assets/js/rickshaw/rickshaw.min.js')); ?>"></script>
+<script src="<?php echo e(asset('neonFrontEnd/assets/js/raphael-min.js')); ?>"></script>
+<script src="<?php echo e(asset('neonFrontEnd/assets/js/morris.min.js')); ?>"></script>
+<script src="<?php echo e(asset('neonFrontEnd/assets/js/jquery.peity.min.js')); ?>"></script>
 <script type="text/javascript">
 ;(function($, window, undefined)
 {
@@ -149,8 +147,8 @@
 			Morris.Donut({
 				element: 'chartLikesComments',
 				data: [
-					{label: "Likes", value: {{$likesAndComments['likes']}}},
-					{label: "Comentarios", value: {{$likesAndComments['comentarios']}}},
+					{label: "Likes", value: <?php echo e($likesAndComments['likes']); ?>},
+					{label: "Comentarios", value: <?php echo e($likesAndComments['comentarios']); ?>},
 
 				],
 				labelColor: '#fafafa',
@@ -161,9 +159,9 @@
 				element: 'chartTOP',
 				axes: true,
 				data: [
-                    @foreach ($usersTOP as $item)
-                    {x: '{{$item['userName']}}', y: {{$item['likes']}}},
-                    @endforeach
+                    <?php $__currentLoopData = $usersTOP; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    {x: '<?php echo e($item['userName']); ?>', y: <?php echo e($item['likes']); ?>},
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 				],
 				xkey: 'x',
 				ykeys: ['y'],
@@ -175,5 +173,7 @@
 
 })(jQuery, window);
 </script>
-<script src="{{ asset('neonFrontEnd/assets/js/jquery.sparkline.min.js') }}"></script>
-@endsection
+<script src="<?php echo e(asset('neonFrontEnd/assets/js/jquery.sparkline.min.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\git\AppTransaccional-v4.2rc\resources\views/chartMongoDB.blade.php ENDPATH**/ ?>
