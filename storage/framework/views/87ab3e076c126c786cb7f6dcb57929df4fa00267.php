4b8916814d119098ceaf80c4b169b6142e5153d0
WPAUL2020 <63327267+WPAUL2020@users.noreply.github.com>
2020-06-02 13:22:19 -0500

    CAMPOS PRODUCTOS

--- storage/framework/views/87ab3e076c126c786cb7f6dcb57929df4fa00267.php
+++ storage/framework/views/87ab3e076c126c786cb7f6dcb57929df4fa00267.php
@@ -8,7 +8,7 @@
 
                 <div class="col-sm-9">
 
-                    <img src="Imagenes/crearproductos.png">
+                    <img src="<?php echo e(asset('Imagenes/crearproductos.png')); ?>">
 
                                 <ol class=" bc-3" >
                             <li>
@@ -25,23 +25,32 @@
 
         </div>
     </section>
-    <div class="container fondo_container ag">
-        <div class="row justify-content-sm-center ">
+<body>
+   <div class="container fondo_container">
+        <div class="row justify-content-sm-center">
             <div class="col-md-5">
                 <h2> <center>REGISTRO DE PRODUCTOS</center></h2>
-                <form method="POST" action="<?php echo e(URL::to('CategoriaCrear/guardar')); ?>" class="form-horizontal"> <?php echo e(csrf_field()); ?>
+                 <div class="form-row">
+            </div>
+            </div>
+        </div>
+        <form method="POST" action="<?php echo e(URL::to('CategoriaCrear/guardar')); ?>" class="form-horizontal"> <?php echo e(csrf_field()); ?>
 
-                <div class="form-group">
+        <div class="form-row">
+            <div class="col">
                 <label>Digitar Producto</label>
                     <input type="text" name="NOMBRE_PRODUCTO_SERV_T" class="form-control" placeholder="NOMBRE DEL PRODUCTO">
-                    <span class="text-danger"><?php echo e($errors->first("NOMBRE_PRODUCTO_SERV_T")); ?></span>
-                    </div>
-                    <div class="form-group">
+                <span class="text-danger"><?php echo e($errors->first("NOMBRE_PRODUCTO_SERV_T")); ?></span>
+            </div>
+         
+            <div class="col">
                 <label>Digitar Detalle del Producto</label>
                     <input type="text" name="DETALLE_PRODUCTO_SERV_T" class="form-control" placeholder="DETALLE DEL PRODUCTO">
                     <span class="text-danger"><?php echo e($errors->first("DETALLE_PRODUCTO_SERV_T")); ?></span>
-                    </div>
-                    <div class="cor">
+            </div>
+       
+        
+                <div class="col">
                     <label for="">Empresa:</label>
                         <select name="ID_EMPRESA_TERCERO" id="ID_EMPRESA_TERCERO" class="form-control be">
                             <?php $__currentLoopData = $empresa; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $empresa): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
@@ -49,17 +58,22 @@
                             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                         </select>
 
-        <span class="text-danger"><?php echo e($errors->first("ID_EMPRESA_TERCERO")); ?></span>
-        </div>
-    </div>
-                    <div class="form-group pro">
-                        <input type="submit" value="Registrar" class="btn btn-primary">
-                        <a class="btn btn-secondary" href="<?php echo e(URL::to('Categorias')); ?>">Regresar</a>
-                    </div>
-                </form>
+                    <span class="text-danger"><?php echo e($errors->first("ID_EMPRESA_TERCERO")); ?></span>
+                
             </div>
         </div>
+<br>
+                    <div class="form-group ">
+                        
+                        <input type="submit" value="Registrar" class="btn btn-primary">
+                       
+                        <a class="btn btn-secondary" href="<?php echo e(URL::to('Categorias')); ?>">Regresar</a>
+                    </div>
+            
+        </form>
     </div>
+       
+
 </body>
 </html>
 <?php $__env->stopSection(); ?>
